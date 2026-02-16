0348aeb2e3496a5ac5af9289810ed7e92b235d06
JonLuca DeCaro <jonluca.decaro@gmail.com>
2026-02-16 15:12:20 -0800

    version

--- app.config.ts
+++ app.config.ts
@@ -1,6 +1,6 @@
 import type { ConfigContext, ExpoConfig } from "expo/config";
 
-const VERSION = "1.0.6";
+const VERSION = "1.0.7";
 
 const getConfig = ({ config }: ConfigContext): ExpoConfig => {
   return {
